0f813311022e4a6767f2cd39230275fdf97b1f8e
LK - MAC <lukas.klingelhoeffer@gmail.com>
2019-12-17 14:14:29 +0100

    weihnachtsbaum bis variante 3

--- drehkreuzsteuerung/drehkreuzsteuerung.cpp
+++ drehkreuzsteuerung/drehkreuzsteuerung.cpp
@@ -3,15 +3,12 @@ using namespace std;
 int main() {
 	cout << "Drehkreuzsteuerung" << endl; 
     cout << "------------------" << endl; 
-    int i;
-    for( i = 30; i>0; i--) {
+    for(int i = 30; i>0; i--) {
         cout << "Noch " << i << " Plätze verfügbar" << endl;   
         cout << "Bitte einsteigen(mit Enter-Taste)" << endl; 
         getchar();
         cout << "Drehkreuz wurde betätigt" << endl;
     } 
-    cout << "I: " << i << endl;
-    
     cout << "Bitte nicht mehr einsteigen, die Bahn ist voll" << endl;   
 	return 0;
 }
---
+++ weihnachtsbaum/weihnachtsbaum.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+
+int main()
+{
+
+    char programmWiederholen;
+    do
+    {
+        short variante = 0;
+        short hoehe = 0;
+        system("cls");
+        std::cout << "--- Weihnachtsbaum" << std::endl;
+
+        std::cout << "Welche Variante soll gezeichnet werden? <1-4>: ";
+        std::cin >> variante;
+        std::cout << std::endl;
+        switch (variante)
+        {
+        case 1:
+            std::cout << "-- Variante 1 --" << std::endl;
+            std::cout << "Höhe des Baumes eingeben <5-40>:";
+            std::cin >> hoehe;
+            std::cout << std::endl;
+            if (hoehe >= 5 && hoehe <= 40)
+            {
+                for (int i = 0; i <= hoehe; i++)
+                {
+                    std::cout << 'x' << std::endl;
+                }
+            }
+            else
+            {
+                std::cout << "Ungültige eingabe";
+            }
+            std::cout << std::endl;
+            break;
+        case 2:
+            std::cout << "-- Variante 2 --" << std::endl;
+            std::cout << "Höhe des Baumes eingeben <5-40>:";
+            std::cin >> hoehe;
+            std::cout << std::endl;
+            if (hoehe >= 5 && hoehe <= 40)
+            {
+                for (int i = 0; i < hoehe; i++)
+                {
+
+                    for (int y = 0; y <= i; y++)
+                    {
+                        std::cout << 'x';
+                    }
+
+                    std::cout << std::endl;
+                }
+            }
+            else
+            {
+                std::cout << "Ungültige eingabe";
+            }
+            std::cout << std::endl;
+            break;
+        case 3:
+            std::cout << "-- Variante 3 --" << std::endl;
+            std::cout << "Höhe des Baumes eingeben <5-40>:";
+            std::cin >> hoehe;
+            std::cout << std::endl;
+            if (hoehe >= 5 && hoehe <= 40)
+            {
+                for (int baumhoehe = 1, baumstufe=0, leerzeichenebene = hoehe; baumhoehe <= hoehe; baumhoehe++, leerzeichenebene--, baumstufe++)
+                {
+                    for (int leerzeichen = leerzeichenebene; leerzeichen >= 0; leerzeichen--)
+                    {
+                        std::cout << ' ';
+                    }
+                    for (int anzahlZeichen = 1+(baumstufe*2); anzahlZeichen >0; anzahlZeichen--)
+                    {
+                        std::cout << 'x';
+                    }
+                    std::cout << std::endl;
+                }
+
+            }
+            else
+            {
+                std::cout << "Ungültige eingabe";
+            }
+            std::cout << std::endl;
+            break;
+
+        case 4:
+
+            break;
+
+        default:
+            std::cout << "Ungültige Eingabe!" << std::endl;
+            break;
+        }
+        std::cout << "Noch ein Baum? <j/n>: ";
+        std::cin >> programmWiederholen;
+        std::cout << std::endl;
+    } while (programmWiederholen == 'j');
+
+    return 0;
+}
